5e8f18007efd90c70d13f6ec5846360bd8973727
Garrett <garrett.dittrich@yahoo.com>
2022-02-03 00:33:38 -0500

    created swipe activity

---
+++ CosAiko/app/src/main/java/com/capstone/cos_aiko/SwipeActivity.java
@@ -0,0 +1,14 @@
+package com.capstone.cos_aiko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SwipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe);
+    }
+}
