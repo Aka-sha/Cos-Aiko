c753a3f5edc5a37f3f18a88d2661d82275909b23
mbittle3 <marc.bittle@gmail.com>
2022-02-21 08:35:44 -0500

    BCrypt

--- Authenticate/Authenticate/src/main/java/com/auth/Authenticate/dao/UserRepository.java
+++ Authenticate/Authenticate/src/main/java/com/auth/Authenticate/dao/UserRepository.java
@@ -1,13 +1,14 @@
 package com.auth.Authenticate.dao;
 
-import com.auth.Authenticate.entity.User;
+import com.auth.Authenticate.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-//import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     // Generates Implementation code for CRUD operations
     // find user by email AND password combination
-    User findByEmailAndPassword(String email, String password);
+    UserEntity findByEmailAndPassword(String email, String password);
+    // findByEmail
+    UserEntity findByEmail(String email);
 }
