7be1045aa2adfec6d0bfe84b35a89b7cb7050c1c
Garrett <garrett.dittrich@yahoo.com>
2022-02-28 19:18:21 -0500

    added picture select activity (incomplete)

---
+++ CosAiko/app/src/main/java/com/capstone/cos_aiko/PictureSelect.java
@@ -0,0 +1,46 @@
+package com.capstone.cos_aiko;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class PictureSelect extends AppCompatActivity {
+    Button addImageButton;
+    FloatingActionButton fab;
+    ImageView firstImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_select);
+        firstImage = (ImageView) findViewById(R.id.first_pic_view);
+        addImageButton = (Button) findViewById(R.id.image_select);
+        fab = (FloatingActionButton) findViewById(R.id.add_pic_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.with(PictureSelect.this)
+                        .crop()
+                        .compress(1024)
+                        .maxResultSize(1080, 1080)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri uri = data.getData();
+        firstImage.setImageURI(uri);
+    }
+}
--- CosAiko/app/src/main/java/com/capstone/cos_aiko/ProfilePage.java
+++ CosAiko/app/src/main/java/com/capstone/cos_aiko/ProfilePage.java
@@ -2,13 +2,27 @@ package com.capstone.cos_aiko;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
 
 public class ProfilePage extends AppCompatActivity {
-
+    Button addPicturesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        addPicturesButton = (Button) findViewById(R.id.add_pic);
+        addPicturesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pictureSelectPage = new Intent(getApplicationContext(), PictureSelect.class);
+                startActivity(pictureSelectPage);
+            }
+        });
     }
 }
